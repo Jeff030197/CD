9e28caf945f4df79a3a2cc1273962d131143a055
Jeff030197 <edwardparias3@gmail.com>
2023-11-29 07:40:43 +0800

    Up

--- src/Menu.js
+++ src/Menu.js
@@ -3,14 +3,14 @@ import ErrorImage from "./assets/images/404.png";
 
 export default function Menu() {
   return (
-    <div className="h-screen flex justify-center items-center">
+    <div className="min-h-screen flex justify-center items-center">
       <div className="w-full px-4 sm:px-6 lg:px-8 max-w-screen-lg text-center">
         <img
           src={ErrorImage}
           alt="404"
-          className="w-full h-auto max-h-screen object-contain"
+          className="w-full h-auto sm:max-h-screen object-contain"
         />
-        <p className="text-5xl mt-6 mb-4"><strong>Under Construction</strong></p>
+        <p className="text-3xl sm:text-5xl mt-6 mb-4"><strong>Under Construction</strong></p>
         <div className="text-center">
           <button className="bg-blue-600 rounded-lg py-2 px-4">
             <Link to={"/"}>
